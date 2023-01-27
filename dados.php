506933fe35f0e5802552eeb4c76b22fcf733a6b6
João Gustavo <92559361+Guga1010@users.noreply.github.com>
2023-01-27 12:53:24 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,42 @@
+<?php
+
+	$vnome = $_GET['fnome'];
+	$qst1= $_GET['questao1'];
+	$qst2 = $_GET['questao2'];
+	$qst3 = $_GET['questao3'];
+	
+	$acertos = 0;
+	
+	if($qst1 == 2){
+		$acertos+=1;
+	}
+	if($qst2 == 5){
+		$acertos+=1;
+	}
+	if($qst3 == 1){
+		$acertos+=1;
+	}
+	
+	include "conexao.inc";
+	
+	$sql = "SELECT * FROM tb_prova WHERE nome = '$vnome'";
+	mysqli_query($con,$sql);
+	$lin = mysqli_affected_rows($con);
+	
+	if($lin == 1){
+		echo "Você já fez a prova!";
+	}else{
+		$sql = "INSERT INTO tb_prova VALUES (NULL,'$vnome',$qst1,$qst2,$qst3,$acertos)";
+		mysqli_query($con,$sql);
+		$lin = mysqli_affected_rows($con);
+		
+		if($lin == 1){
+			echo "Registrado!<br/>";
+		}else{
+			echo "Falha no registro!<br/>";
+		}
+	}
+	
+	mysqli_close($con);
+	
+?>
