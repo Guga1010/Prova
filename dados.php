1ee3b1ee47dbcd9c5535c983bf07d5f85d549a8c
João Gustavo <92559361+Guga1010@users.noreply.github.com>
2023-02-16 08:28:11 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -19,14 +19,14 @@
 	
 	include "conexao.inc";
 	
-	$sql = "SELECT * FROM tb_prova WHERE nome = '$vnome'";
+	$sql = "SELECT * FROM tb_prova WHERE nome = '$vnome'"; //Alterar, caso tenha criado uma tabela com nome diferente
 	mysqli_query($con,$sql);
 	$lin = mysqli_affected_rows($con);
 	
 	if($lin == 1){
 		echo "Você já fez a prova!";
 	}else{
-		$sql = "INSERT INTO tb_prova VALUES (NULL,'$vnome',$qst1,$qst2,$qst3,$acertos)";
+		$sql = "INSERT INTO tb_prova VALUES (NULL,'$vnome',$qst1,$qst2,$qst3,$acertos)"; //Alterar, caso tenha criado uma tabela com nome diferente
 		mysqli_query($con,$sql);
 		$lin = mysqli_affected_rows($con);
 		
